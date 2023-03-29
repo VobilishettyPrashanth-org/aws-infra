d01679ea644c0cd9113b4ac735ceb74f8435406b
VobilishettyPrashanth <vobilishetty.p@northeastern.edu>
2023-03-28 21:01:18 -0400

    format terraform files

--- environment/demo/main.tf
+++ environment/demo/main.tf
@@ -15,10 +15,10 @@ module "web_app" {
   source = "../../web-app-module"
 
   # Input Variables.
-  cidr_name        = var.cidr_name
-  vpc_tag_name     = var.vpc_tag_name
-  aws_region       = var.aws_region
-  aws_subnet_count = var.aws_subnet_count
+  cidr_name         = var.cidr_name
+  vpc_tag_name      = var.vpc_tag_name
+  aws_region        = var.aws_region
+  aws_subnet_count  = var.aws_subnet_count
   my_ami            = var.my_ami
   db_storage_size   = var.db_storage_size
   db_instance_class = var.db_instance_class
